1c8fb9a1bd8e0c8ac1554569ebd8707afaff631d
Mgenius3 <benmos16@gmail.com>
2024-11-23 05:04:25 +0100

    update

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,14 +1,20 @@
+'use client'
+
+import { useState } from "react";
 import styles from "./style.module.css";
 import Link from "next/link";
 
 export default function Header({ active_no }: { active_no: Number }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className={styles.main}>
-      <main className="flex justify-around items-end">
+      <main className="flex justify-between items-center">
         <div className="sm:p-2">
-          <img src="/logo.svg" />
+          <img src="/logo.svg" alt="Logo" />
         </div>
-        <div className={styles.link}>
+
+        <div className={`${styles.link} ${menuOpen ? styles.openMenu : ""}`}>
           <Link href={"/"}>
             <p className={`${active_no == 0 ? styles.active : ""}`}>Home</p>
           </Link>
@@ -16,10 +22,6 @@ export default function Header({ active_no }: { active_no: Number }) {
           <Link href={"/about_us"}>
             <p className={`${active_no == 1 ? styles.active : ""}`}>About</p>
           </Link>
-
-          {/* <Link href={"/services"}>
-            <p className={`${active_no == 2 ? styles.active : ""}`}>Services</p>
-          </Link> */}
 
           <Link href={"/contact"}>
             <p className={`${active_no == 3 ? styles.active : ""}`}>Contact</p>
@@ -30,8 +32,8 @@ export default function Header({ active_no }: { active_no: Number }) {
           </Link>
         </div>
 
-        <div className={styles.menu}>
-          <img src='/menu.svg'/>
+        <div className={styles.menu} onClick={() => setMenuOpen(!menuOpen)}>
+          <img src="/menu.svg" alt="Menu Icon" />
         </div>
       </main>
     </header>
